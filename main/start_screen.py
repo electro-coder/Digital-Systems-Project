8f89a85326af1a3d7e52791ba657a02e0752072f
Prakhar Gupta <prakhargupta1811@gmail.com>
2023-10-22 15:50:12 +0530

    test

--- main/start_screen.py
+++ main/start_screen.py
@@ -4,18 +4,18 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption("CodeDiffuse")
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load('../Resources/icon.png')
 pygame.display.set_icon(icon)
 
-playerimg = pygame.image.load('player2.png')
-playerX = 100;
-playerY = 150;
+playerimg = pygame.image.load('../Resources/player2.png')
+playerX = 100
+playerY = 150
 
 def player(x,y):
     screen.blit(playerimg,(x,y))
 
-x = playerX;
-y = playerY;
+x = playerX
+y = playerY
 running = True
 change = 0
 while running:
@@ -40,6 +40,4 @@ while running:
     if x>736:
         x = 736
     player(x, y)
-
-
     pygame.display.update()
