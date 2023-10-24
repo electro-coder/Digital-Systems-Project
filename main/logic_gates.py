583c489b9da2d2eb70f3f33a74c3247505f6eea4
Ayush Gupta <146838078+ayushhwho@users.noreply.github.com>
2023-10-24 10:09:29 +0530

    Update logic_gates.py

--- main/logic_gates.py
+++ main/logic_gates.py
@@ -121,6 +121,50 @@ class NOTGate:
         self.input1 = input1
         self.output = not input1
 
+class XORGate:
+    def __init__(self, x=150, y=80, input1=False, input2=False):
+        self.x = x
+        self.y = y
+        self.width = 50
+        self.height = 40
+        self.input1 = input1
+        self.input2 = input2
+        self.output = input1 ^ input2
+
+    def draw(self):
+        pygame.draw.rect(screen, WHITE, (self.x, self.y, self.width, self.height))
+        pygame.draw.line(screen, BLACK, (self.x, self.y + self.height // 2), (self.x + self.width, self.y + self.height // 2))
+        pygame.draw.circle(screen, BLACK, (self.x - 20, self.y + self.height // 2), 6)
+        pygame.draw.circle(screen, BLACK, (self.x - 20, self.y + self.height // 2 + 40), 6)
+        pygame.draw.circle(screen, BLACK, (self.x + self.width + 20, self.y + self.height // 2), 6)
+
+    def set_input(self, input1, input2):
+        self.input1 = input1
+        self.input2 = input2
+        self.output = input1 ^ input2
+
+class XNORGate:
+    def __init__(self, x=150, y=80, input1=False, input2=False):
+        self.x = x
+        self.y = y
+        self.width = 50
+        self.height = 40
+        self.input1 = input1
+        self.input2 = input2
+        self.output = not (input1 ^ input2)
+
+    def draw(self):
+        pygame.draw.rect(screen, WHITE, (self.x, self.y, self.width, self.height))
+        pygame.draw.line(screen, BLACK, (self.x, self.y + self.height // 2), (self.x + self.width, self.y + self.height // 2))
+        pygame.draw.circle(screen, BLACK, (self.x - 20, self.y + self.height // 2), 6)
+        pygame.draw.circle(screen, BLACK, (self.x - 20, self.y + self.height // 2 + 40), 6)
+        pygame.draw.circle(screen, BLACK, (self.x + self.width + 20, self.y + self.height // 2), 6)
+
+    def set_input(self, input1, input2):
+        self.input1 = input1
+        self.input2 = input2
+        self.output = not (input1 ^ input2) 
+
 # Create AND gate instances with overloaded constructors
 and_gate1 = ANDGate()  # Uses default values (x=150, y=80, input1=False, input2=False)
 and_gate2 = ANDGate(50, 30, True, True)  # Custom values (x=50, y=30, input1=True, input2=True)
