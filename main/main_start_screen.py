4dd88e03aad05a7f287e74fa35831587e827fcdd
ayushhwho <ee22b005@iittp.ac.in>
2023-11-07 22:54:02 +0530

    start screen started

--- main/main_start_screen.py
+++ main/main_start_screen.py
@@ -1,13 +1,60 @@
 import pygame
 import sys
-import random
 
-class start_screen:
-    def __init__(self, screen):
+class StartScreen:
+    def __init__(self, width, height, title, font_size=40):
+        self.width = width
+        self.height = height
+        self.title = title
+        self.font_size = font_size
 
-        #Initialize pygame
-        pygame.init()
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(title)
 
-        #Display Surface
-        self.screen=screen
-        pygame.display.set_caption("CodeDiffuse")
+        self.font = pygame.font.Font(None, font_size)
+
+        path_background="../Resources/nand.png"
+
+        try:
+            self.background=pygame.image.load(path_background)
+        except(FileNotFoundError):
+            self.background=pygame.image.load(path_background.replace("..","."))
+
+        self.background = pygame.transform.scale(self.background, (width, height))
+    
+
+    def draw_text(self, text, x, y):
+        text_surface = self.font.render(text, True, (255, 255, 255))
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        running = False
+
+            self.screen.fill((0, 0, 0))
+
+            self.screen.blit(self.background, (0, 0))
+
+            self.draw_text(self.title, self.width // 2, self.height // 4)
+            self.draw_text("Press SPACE to Start", self.width // 2, self.height // 2)
+
+            
+
+            pygame.display.flip()
+
+        pygame.quit()
+        sys.exit()
+
+# Example usage
+if __name__ == "__main__":
+    pygame.init()
+    start_screen = StartScreen(800, 600, "Game Start Screen")
+    start_screen.run()
